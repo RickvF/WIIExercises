e60a4a0f88f2525ae07143ba3932a9bfc048c344
Sascha Worms <Sascha Worms>
2016-05-18 15:03:18 +0200

    pokemon loader

--- src/NetwerkProgrammerenSascha_Rick/Pokemon.java
+++ src/NetwerkProgrammerenSascha_Rick/Pokemon.java
@@ -1,4 +1,3 @@
-package NetwerkProgrammerenSascha_Rick;
 
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
@@ -10,98 +9,102 @@ import java.io.Serializable;
 import java.nio.file.FileSystemNotFoundException;
 import java.util.ArrayList;
 
-
-
 public class Pokemon implements Comparable<String>, Serializable
 {
-	private BufferedImage image;
-	private ArrayList<String> types;
-	private String name;
-	private int generation;
-	private String color;
-	
-	//Constructor of the class Pokemon
-	public Pokemon(BufferedImage image, String name, int generation, String color)
-	{
-		this.image = image;
-		this.name = name;
-		this.generation = generation;
-		this.color = color;
-	}
-	
-	//method to add a type to the list of types
-	public void addType(String type)
-	{
-		types.add(type);
-	}
-	
-	//method which returns the pokemon's image
-	public BufferedImage getImage()
-	{
-		return image;
-	}
-	
-	//method which returns the arraylist with types 
-	public ArrayList<String> getTypes()
-	{
-		return types;
-	}
-	
-	//method which returns the pokemon's name
-	public String getName()
-	{
-		return name;
-	}
-	
-	//method which returns the pokemon's generation
-	public int getGeneration()
-	{
-		return generation;
-	}
-	
-	//method which returns the pokemon's color
-	public String getColor()
-	{
-		return color;
-	}
+    private ArrayList<String> types;
+    private String name;
+    private int generation;
+    private String color;
 
-	@Override
-	public int compareTo(String t)
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	public void saveSchedule()throws FileSystemNotFoundException{
-		try {
-			FileOutputStream fos = new FileOutputStream("Pokemon");
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(this);
-			oos.close();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	static String pokomen = "Bulbasaur";
-	
-	public static Pokemon load()throws FileNotFoundException{
-		try{
-			FileInputStream fis = new FileInputStream(pokomen);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			
-			Object object = ois.readObject();
-			ois.close();
-			if(object instanceof Pokemon){
-				System.out.println("Pokemon has loaded.");
-				Pokemon temp = (Pokemon) object;
-								
-				return temp;
-			}
-			return null;
-		}
-		catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
+    //Constructor of the class Pokemon
+    public Pokemon(String name, int generation, String color, ArrayList<String> types)
+    {
+        this.types = types;
+        this.name = name;
+        this.generation = generation;
+        this.color = color;
+    }
+
+    public Pokemon(String pokemonName)
+    {
+        try{
+            Pokemon temp = load(pokemonName);
+            this.types = temp.getTypes();
+            this.name = temp.getName();
+            this.generation = temp.getGeneration();
+            this.color = temp.getColor();
+        }
+        catch(Exception e){}
+
+    }
+
+    //method to add a type to the list of types
+    public void addType(String type)
+    {
+        types.add(type);
+    }
+
+    //method which returns the arraylist with types 
+    public ArrayList<String> getTypes()
+    {
+        return types;
+    }
+
+    //method which returns the pokemon's name
+    public String getName()
+    {
+        return name;
+    }
+
+    //method which returns the pokemon's generation
+    public int getGeneration()
+    {
+        return generation;
+    }
+
+    //method which returns the pokemon's color
+    public String getColor()
+    {
+        return color;
+    }
+
+    @Override
+    public int compareTo(String t)
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public void savePokemon()throws FileSystemNotFoundException{
+        try {
+            FileOutputStream fos = new FileOutputStream(name+ ".pkm");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+            oos.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Pokemon load(String pokemon)throws FileNotFoundException{
+        try{
+            FileInputStream fis = new FileInputStream(pokemon+".pkm");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Object object = ois.readObject();
+            ois.close();
+            if(object instanceof Pokemon){
+                System.out.println(pokemon +" has been caught succesfully");
+                Pokemon temp = (Pokemon) object;
+
+                return temp;
+            }
+            return null;
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
